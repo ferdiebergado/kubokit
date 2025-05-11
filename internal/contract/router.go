e86d79e5e2ea8b4febc978b7d4c70603244fc5f7
ferdie bergado <ferdiebergado@gmail.com>
2025-05-11 16:51:02 +0800

    feat: Add route groups

--- internal/contract/router.go
+++ internal/contract/router.go
@@ -14,4 +14,5 @@ type Router interface {
 	Patch(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler)
 	Delete(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler)
 	Options(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler)
+	Group(pattern string, handlerFunc func(r Router), middlewares ...func(next http.Handler) http.Handler)
 }
--- internal/http/goexpress_router.go
+++ internal/http/goexpress_router.go
@@ -2,9 +2,13 @@ package http
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/ferdiebergado/goexpress"
+	"github.com/ferdiebergado/slim/internal/contract"
 )
+
+var _ contract.Router = &GoexpressRouter{}
 
 type GoexpressRouter struct {
 	handler *goexpress.Router
@@ -16,27 +20,33 @@ func NewGoexpressRouter() *GoexpressRouter {
 	}
 }
 
-func (r *GoexpressRouter) Delete(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Delete(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Delete(pattern, handlerFunc, middlewares...)
 }
 
-func (r *GoexpressRouter) Get(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Get(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Get(pattern, handlerFunc, middlewares...)
 }
 
-func (r *GoexpressRouter) Options(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Options(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Options(pattern, handlerFunc, middlewares...)
 }
 
-func (r *GoexpressRouter) Patch(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Patch(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Patch(pattern, handlerFunc, middlewares...)
 }
 
-func (r *GoexpressRouter) Post(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Post(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Post(pattern, handlerFunc, middlewares...)
 }
 
-func (r *GoexpressRouter) Put(pattern string, handlerFunc http.HandlerFunc, middlewares ...func(next http.Handler) http.Handler) {
+func (r *GoexpressRouter) Put(pattern string, handlerFunc http.HandlerFunc,
+	middlewares ...func(next http.Handler) http.Handler) {
 	r.handler.Put(pattern, handlerFunc, middlewares...)
 }
 
@@ -46,4 +56,16 @@ func (r *GoexpressRouter) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 
 func (r *GoexpressRouter) Use(middleware func(next http.Handler) http.Handler) {
 	r.handler.Use(middleware)
+}
+
+func (r *GoexpressRouter) Group(prefix string, handlerFunc func(r contract.Router),
+	middlewares ...func(next http.Handler) http.Handler) {
+	handlerFunc(r)
+
+	prefix = strings.TrimSuffix(prefix, "/")
+	if prefix == "" {
+		prefix = "/"
+	}
+
+	r.handler.Handle(prefix+"/", http.StripPrefix(prefix, r), middlewares...)
 }
