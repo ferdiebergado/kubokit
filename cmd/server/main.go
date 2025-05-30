8d0e50511ca6143ea838ed3d354b3f7d570ccb37
ferdie bergado <ferdiebergado@gmail.com>
2025-05-30 21:36:45 +0800

    refactor: Call stop() before os.Exit()

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -15,8 +15,11 @@ func main() {
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 	defer stop()
 
+	slog.Info("Starting server...")
 	if err := app.Run(ctx); err != nil {
 		slog.Error("Application failed to start.", "reason", err)
+		stop()
 		os.Exit(1)
 	}
+	slog.Info("Server shutdown gracefully.")
 }
