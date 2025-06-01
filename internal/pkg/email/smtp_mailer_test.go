b6c9e4c8273ecac04e9cedd8a18f15d233004965
ferdie bergado <ferdiebergado@gmail.com>
2025-06-01 14:53:56 +0800

    test: Add SMTPMailer SendHTML test

---
+++ internal/pkg/email/smtp_mailer_test.go
@@ -0,0 +1,47 @@
+package email_test
+
+import (
+	"os"
+	"strconv"
+	"testing"
+
+	"github.com/ferdiebergado/gopherkit/env"
+	"github.com/ferdiebergado/kubokit/internal/config"
+	"github.com/ferdiebergado/kubokit/internal/pkg/email"
+)
+
+func TestSMTPMailer_SendHTML(t *testing.T) {
+	t.Parallel()
+	const rootPath = "../../../"
+	if err := env.Load(rootPath + ".env.testing"); err != nil {
+		t.Fatal(err)
+	}
+
+	port, err := strconv.Atoi(os.Getenv("SMTP_PORT"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	user := os.Getenv("SMTP_USER")
+	smtpCfg := &email.SMTPConfig{
+		Host:     os.Getenv("SMTP_HOST"),
+		Port:     port,
+		User:     user,
+		Password: os.Getenv("STMP_PASS"),
+	}
+
+	emailCfg := &config.Email{
+		Templates: rootPath + "web/templates",
+		Layout:    "layout.html",
+		Sender:    user,
+	}
+	mailer, err := email.NewSMTPMailer(smtpCfg, emailCfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	to := []string{"test@example.com"}
+	if err := mailer.SendHTML(to, "test", "verification", nil); err != nil {
+		t.Errorf("\nwant: nil error\ngot: %v", err)
+	}
+}
