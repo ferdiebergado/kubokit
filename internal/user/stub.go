61d00db44edc31bb5dadd3502a019163aa0fa1ac
ferdie bergado <ferdiebergado@gmail.com>
2025-06-12 12:55:57 +0800

    refactor: Update return value of FindUserByEmail

--- internal/user/stub.go
+++ internal/user/stub.go
@@ -36,7 +36,7 @@ func (s *StubService) FindUserByEmail(ctx context.Context, email string) (*User,
 type StubRepo struct {
 	ListUsersFunc       func(ctx context.Context) ([]User, error)
 	CreateUserFunc      func(ctx context.Context, params CreateUserParams) (User, error)
-	FindUserByEmailFunc func(ctx context.Context, email string) (User, error)
+	FindUserByEmailFunc func(ctx context.Context, email string) (*User, error)
 }
 
 func (r *StubRepo) CreateUser(ctx context.Context, params CreateUserParams) (User, error) {
@@ -53,9 +53,9 @@ func (r *StubRepo) ListUsers(ctx context.Context) ([]User, error) {
 	return r.ListUsersFunc(ctx)
 }
 
-func (r *StubRepo) FindUserByEmail(ctx context.Context, email string) (User, error) {
+func (r *StubRepo) FindUserByEmail(ctx context.Context, email string) (*User, error) {
 	if r.FindUserByEmailFunc == nil {
-		return User{}, errors.New("FindUserByEmail not implemented by stub")
+		return nil, errors.New("FindUserByEmail not implemented by stub")
 	}
 	return r.FindUserByEmailFunc(ctx, email)
 }
