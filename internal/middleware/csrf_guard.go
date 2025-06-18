77d9284704574ad83b9071c9c86de65ed0ce3aaf
ferdie bergado <ferdiebergado@gmail.com>
2025-06-18 18:45:48 +0800

    refactor: Inject Randomizer as dependency

--- internal/middleware/csrf_guard.go
+++ internal/middleware/csrf_guard.go
@@ -13,60 +13,62 @@ import (
 )
 
 const (
-	csrfCookieName  = "csrf_token"
-	csrfHeaderName  = "X-CSRF-Token"
+	CookieCSRF      = "csrf_token"
+	HeaderCSRF      = "X-CSRF-Token"
 	csrfTokenLength = 32
 )
 
-func CSRFGuard(next http.Handler) http.Handler {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		cookie, err := r.Cookie(csrfCookieName)
-		// On safe methods, set token if missing
-		if r.Method == http.MethodGet || r.Method == http.MethodHead || r.Method == http.MethodOptions {
-			var token string
-			if err != nil || cookie.Value == "" {
-				b, randErr := security.GenerateRandomBytes(csrfTokenLength)
-				if err != nil {
-					web.RespondInternalServerError(w, randErr)
-					return
+func CSRFGuard(randomizer security.Randomizer) func(http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			cookie, err := r.Cookie(CookieCSRF)
+			// On safe methods, set token if missing
+			if r.Method == http.MethodGet || r.Method == http.MethodHead || r.Method == http.MethodOptions {
+				var token string
+				if err != nil || cookie.Value == "" {
+					b, randErr := randomizer.GenerateRandomBytes(csrfTokenLength)
+					if randErr != nil {
+						web.RespondInternalServerError(w, randErr)
+						return
+					}
+					token = base64.RawURLEncoding.EncodeToString(b)
+					http.SetCookie(w, &http.Cookie{
+						Name:     CookieCSRF,
+						Value:    token,
+						Path:     "/",
+						HttpOnly: true,
+						Secure:   true,
+						SameSite: http.SameSiteStrictMode,
+						Expires:  time.Now().Add(24 * time.Hour),
+					})
+				} else {
+					token = cookie.Value
 				}
-				token = base64.RawURLEncoding.EncodeToString(b)
-				http.SetCookie(w, &http.Cookie{
-					Name:     csrfCookieName,
-					Value:    token,
-					Path:     "/",
-					HttpOnly: true,
-					Secure:   true,
-					SameSite: http.SameSiteStrictMode,
-					Expires:  time.Now().Add(24 * time.Hour),
-				})
-			} else {
-				token = cookie.Value
-			}
-			// Optionally expose the token to JS clients via header
-			w.Header().Set(csrfHeaderName, token)
-			next.ServeHTTP(w, r)
-			return
-		}
-
-		// On unsafe methods, validate token
-		if err != nil || cookie.Value == "" {
-			web.RespondForbidden(w, errors.New("CSRF token missing"), message.InvalidInput, nil)
-			return
-		}
-		sentToken := r.Header.Get(csrfHeaderName)
-		if sentToken == "" {
-			// Try to read from form data (for HTML forms)
-			if err := r.ParseForm(); err != nil {
-				web.RespondBadRequest(w, err, "Invalid form data.", nil)
+				// Optionally expose the token to JS clients via header
+				w.Header().Set(HeaderCSRF, token)
+				next.ServeHTTP(w, r)
 				return
 			}
-			sentToken = r.FormValue(csrfCookieName)
-		}
-		if subtle.ConstantTimeCompare([]byte(cookie.Value), []byte(sentToken)) == 0 {
-			web.RespondForbidden(w, errors.New("invalid CSRF token"), message.InvalidInput, nil)
-			return
-		}
-		next.ServeHTTP(w, r)
-	})
+
+			// On unsafe methods, validate token
+			if err != nil || cookie.Value == "" {
+				web.RespondForbidden(w, errors.New("CSRF token missing"), message.InvalidInput, nil)
+				return
+			}
+			sentToken := r.Header.Get(HeaderCSRF)
+			if sentToken == "" {
+				// Try to read from form data (for HTML forms)
+				if err := r.ParseForm(); err != nil {
+					web.RespondBadRequest(w, err, "Invalid form data.", nil)
+					return
+				}
+				sentToken = r.FormValue(CookieCSRF)
+			}
+			if subtle.ConstantTimeCompare([]byte(cookie.Value), []byte(sentToken)) == 0 {
+				web.RespondForbidden(w, errors.New("invalid CSRF token"), message.InvalidInput, nil)
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
 }
