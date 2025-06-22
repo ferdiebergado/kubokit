dbedbbe9c9bd11cf98f98d98bd2ec3d405b241d1
ferdie bergado <ferdiebergado@gmail.com>
2025-06-22 13:06:19 +0800

    test: Add unit test for LoginUser

--- internal/auth/handler_test.go
+++ internal/auth/handler_test.go
@@ -13,6 +13,7 @@ import (
 	"github.com/ferdiebergado/kubokit/internal/config"
 	"github.com/ferdiebergado/kubokit/internal/model"
 	"github.com/ferdiebergado/kubokit/internal/pkg/security"
+	timex "github.com/ferdiebergado/kubokit/internal/pkg/time"
 	"github.com/ferdiebergado/kubokit/internal/pkg/web"
 	"github.com/ferdiebergado/kubokit/internal/platform/jwt"
 	"github.com/ferdiebergado/kubokit/internal/user"
@@ -110,6 +111,85 @@ func TestHandler_RegisterUser(t *testing.T) {
 					t.Errorf("apiRes.Data = %+v, want: %+v", gotUser, wantUser)
 				}
 			}
+		})
+	}
+}
+
+func TestHandler_LoginUser(t *testing.T) {
+	t.Parallel()
+	const (
+		testEmail = "test@example.com"
+		testPass  = "test"
+		timeUnit  = time.Minute
+	)
+
+	defaultDuration := 30 * timeUnit
+
+	tests := []struct {
+		name       string
+		input      auth.UserLoginRequest
+		code       int
+		loginFunc  func(ctx context.Context, params auth.LoginUserParams) (accessToken, refreshToken string, err error)
+		verifyFunc func(tokenString string) (string, error)
+		bakeFunc   func() (*http.Cookie, error)
+	}{
+		{
+			name: "Registered user with verified email",
+			input: auth.UserLoginRequest{
+				Email:    testEmail,
+				Password: testPass,
+			},
+			code: http.StatusOK,
+			loginFunc: func(ctx context.Context, params auth.LoginUserParams) (accessToken, refreshToken string, err error) {
+				return "access_token", "refresh_token", nil
+			},
+			verifyFunc: func(tokenString string) (string, error) {
+				return testEmail, nil
+			},
+			bakeFunc: func() (*http.Cookie, error) {
+				return &http.Cookie{}, nil
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			svc := &auth.StubService{
+				LoginUserFunc: tc.loginFunc,
+			}
+			signer := &jwt.StubSigner{
+				VerifyFunc: tc.verifyFunc,
+			}
+			cfg := &config.Config{
+				Cookie: &config.Cookie{
+					Name:   "refresh_token",
+					MaxAge: timex.Duration{Duration: defaultDuration},
+				},
+			}
+			baker := &security.StubBaker{
+				BakeFunc: tc.bakeFunc,
+			}
+			providers := &auth.Providers{
+				Cfg:     cfg,
+				DB:      nil,
+				Hasher:  nil,
+				Signer:  signer,
+				Mailer:  nil,
+				UserSvc: nil,
+				Baker:   baker,
+				TXMgr:   nil,
+			}
+			authHandler := auth.NewHandler(svc, providers)
+
+			ctx := web.NewContextWithParams(context.Background(), tc.input)
+			req := httptest.NewRequestWithContext(ctx, http.MethodPost, "/", http.NoBody)
+			rec := httptest.NewRecorder()
+			authHandler.LoginUser(rec, req)
+
+			gotCode, wantCode := rec.Code, tc.code
+			if gotCode != wantCode {
+				t.Errorf("rec.Code = %d, want: %d", gotCode, wantCode)
+			}
+
 		})
 	}
 }
