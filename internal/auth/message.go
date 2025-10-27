4fe903abcfa1d2dd046c2184a531759ac1a72094
ferdie bergado <ferdiebergado@gmail.com>
2025-10-27 21:20:58 +0800

    refactor: Add password change success message

--- internal/auth/message.go
+++ internal/auth/message.go
@@ -1,14 +1,15 @@
 package auth
 
 const (
-	MsgLoggedIn             = "Logged in."
-	MsgRefreshed            = "Token refreshed."
-	MsgNotVerified          = "Email not yet verified."
-	MsgVerifySuccess        = "Verification complete. You can now login."
-	MsgRegisterSuccess      = "Thank you for registering. A verification link was sent to your email."
-	MsgReVerifySuccess      = "A verification link was sent to your email."
-	MsgFmtFindUserByEmail   = "find user by email: %w"
-	MsgFmtFindUser          = "find user: %w"
-	MsgPasswordResetSuccess = "Password was reset successfully!"
-	MsgUserExists           = "User already exists."
+	MsgLoggedIn               = "Logged in."
+	MsgRefreshed              = "Token refreshed."
+	MsgNotVerified            = "Email not yet verified."
+	MsgVerifySuccess          = "Verification complete. You can now login."
+	MsgRegisterSuccess        = "Thank you for registering. A verification link was sent to your email."
+	MsgReVerifySuccess        = "A verification link was sent to your email."
+	MsgFmtFindUserByEmail     = "find user by email: %w"
+	MsgFmtFindUser            = "find user: %w"
+	MsgPasswordResetSuccess   = "Password was reset successfully!"
+	MsgUserExists             = "User already exists."
+	MsgSuccessPasswordChanged = "Password changed successfully!"
 )
