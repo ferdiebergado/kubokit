d5ee192617cc22cf780647b91429cdfd614f010a
ferdie bergado <ferdiebergado@gmail.com>
2025-10-29 08:54:25 +0800

    refactor: Validate the content type in RFC-compliant way

--- internal/middleware/check_contenttype.go
+++ internal/middleware/check_contenttype.go
@@ -3,6 +3,7 @@ package middleware
 import (
 	"fmt"
 	"log/slog"
+	"mime"
 	"net/http"
 
 	"github.com/ferdiebergado/kubokit/internal/pkg/message"
@@ -15,7 +16,8 @@ func CheckContentType(next http.Handler) http.Handler {
 		contentType := r.Header.Get(web.HeaderContentType)
 
 		if r.Method == http.MethodPost || r.Method == http.MethodPut || r.Method == http.MethodPatch {
-			if contentType != web.MimeJSON {
+			mediaType, _, err := mime.ParseMediaType(contentType)
+			if err != nil || mediaType != web.MimeJSON {
 				web.RespondUnsupportedMediaType(w, fmt.Errorf("unsupported content-type: %s", contentType), message.InvalidInput, nil)
 				return
 			}
