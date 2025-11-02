06c2f31fb1180a959ec9d936514fb2776b09cda1
ferdie bergado <ferdiebergado@gmail.com>
2025-11-02 18:45:32 +0800

    refactor: Create TestMain for user package

---
+++ internal/user/user_test.go
@@ -0,0 +1,15 @@
+package user_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/ferdiebergado/kubokit/internal/pkg/logging"
+)
+
+func TestMain(t *testing.M) {
+	logging.SetupLogger("testing", "error", os.Stdout)
+
+	code := t.Run()
+	os.Exit(code)
+}
