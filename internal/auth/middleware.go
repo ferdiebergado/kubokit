c718ea7de79566d674dffd344c2fd031295886bc
ferdie bergado <ferdiebergado@gmail.com>
2025-11-11 12:51:36 +0800

    refactor: Remove VerifyToken

--- internal/auth/middleware.go
+++ internal/auth/middleware.go
@@ -8,35 +8,6 @@ import (
 	"github.com/ferdiebergado/kubokit/internal/pkg/web"
 )
 
-// VerifyToken verifies if the token in the url query string is valid.
-func VerifyToken(signer Signer) func(http.Handler) http.Handler {
-	return func(next http.Handler) http.Handler {
-		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			token := r.URL.Query().Get("token")
-			if token == "" {
-				web.RespondUnauthorized(w, ErrInvalidToken, MsgInvalidUser, nil)
-				return
-			}
-
-			claims, err := signer.Verify(token)
-			if err != nil {
-				web.RespondUnauthorized(w, ErrInvalidToken, MsgInvalidUser, nil)
-				return
-			}
-
-			userID, ok := claims["sub"].(string)
-			if !ok {
-				web.RespondUnauthorized(w, ErrInvalidToken, MsgInvalidUser, nil)
-				return
-			}
-
-			ctx := ContextWithUser(r.Context(), userID)
-			r = r.WithContext(ctx)
-			next.ServeHTTP(w, r)
-		})
-	}
-}
-
 func RequireToken(signer Signer) func(http.Handler) http.Handler {
 	return func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
